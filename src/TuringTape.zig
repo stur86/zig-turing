d9d23754fd58bd98a0a4e0cf5868ea48dbb3da71
Simone Sturniolo <simonesturniolo@gmail.com>
2024-04-01 14:51:38 +0100

    Simplified TuringTape

--- src/TuringTape.zig
+++ src/TuringTape.zig
@@ -4,127 +4,83 @@ const Allocator = std.mem.Allocator;
 
 const TuringModelError = @import("TuringModelError.zig").TuringModelError;
 
-const TuringTapeBlock = struct {
-    from_idx: i128,
-    to_idx: i128,
-    items: []u8 = undefined,
-    prev: ?*TuringTapeBlock = null,
-    next: ?*TuringTapeBlock = null,
-
-    pub fn init(idx0: i128, idx1: i128) TuringTapeBlock {
-        return .{ .from_idx = idx0, .to_idx = idx1 };
-    }
-
-    pub fn build(self: *TuringTapeBlock, allocator: Allocator) !void {
-        if (self.to_idx <= self.from_idx) {
-            return TuringModelError.IndexError;
-        }
-        const bsize: usize = @intCast(self.to_idx - self.from_idx);
-        self.items = try allocator.alloc(u8, bsize);
-    }
-
-    pub fn deinit(self: TuringTapeBlock, allocator: std.mem.Allocator) void {
-        allocator.free(self.items);
-    }
-};
+const TuringTapeAddress = struct { i: usize, is_left: bool };
 
 const TuringTape = struct {
-    idx: i128 = 0,
-    start_idx: i128 = 0,
     allocator: Allocator,
-    block_size: usize,
-    first_idx: i128 = 0,
-    first_block: ?*TuringTapeBlock = null,
-    last_idx: i128 = 0,
-    last_block: ?*TuringTapeBlock = null,
-    all_blocks: std.ArrayList(*TuringTapeBlock),
+    tape_rh: std.ArrayList(u8),
+    tape_lh: std.ArrayList(u8),
 
-    pub fn init(allocator: Allocator, block_size: usize) !TuringTape {
-        return .{ .allocator = allocator, .block_size = block_size, .all_blocks = std.ArrayList(*TuringTapeBlock).init(allocator) };
+    pub fn init(allocator: Allocator) TuringTape {
+        const tape_rh = std.ArrayList(u8).init(allocator);
+        const tape_lh = std.ArrayList(u8).init(allocator);
+        return .{ .allocator = allocator, .tape_rh = tape_rh, .tape_lh = tape_lh };
     }
 
-    fn insertFirstBlock(self: *TuringTape, blockp: *TuringTapeBlock) void {
-        self.first_block = blockp;
-        self.last_block = blockp;
-    }
-
-    pub fn appendBlock(self: *TuringTape) !void {
-        const blockp = try self.allocator.create(TuringTapeBlock);
-        blockp.* = TuringTapeBlock.init(self.last_idx, self.last_idx + self.block_size);
-        try self.all_blocks.append(blockp);
-        try blockp.build(self.allocator);
-        self.last_idx = blockp.to_idx;
-        if (self.last_block == null) {
-            self.insertFirstBlock(blockp);
+    pub fn tape_address(idx: i128) TuringTapeAddress {
+        if (idx >= 0) {
+            return .{ .i = @intCast(idx), .is_left = false };
         } else {
-            self.last_block.?.next = blockp;
-            blockp.prev = self.last_block;
-            self.last_block = blockp;
+            return .{ .i = @intCast(-1 - idx), .is_left = true };
         }
     }
 
-    pub fn prependBlock(self: *TuringTape) !void {
-        const blockp = try self.allocator.create(TuringTapeBlock);
-        blockp.* = TuringTapeBlock.init(self.first_idx - self.block_size, self.first_idx);
-        try self.all_blocks.append(blockp);
-        try blockp.build(self.allocator);
-        self.first_idx = blockp.from_idx;
-        if (self.first_block == null) {
-            self.insertFirstBlock(blockp);
-        } else {
-            self.first_block.?.prev = blockp;
-            blockp.next = self.first_block;
-            self.first_block = blockp;
+    pub fn getAddr(self: *TuringTape, idx: i128) !*u8 {
+        const addr = TuringTape.tape_address(idx);
+        const tape: *std.ArrayList(u8) = if (addr.is_left) &(self.tape_lh) else &(self.tape_rh);
+        // Extend if necessary
+
+        if (tape.items.len < addr.i + 1) {
+            const n: usize = addr.i - tape.items.len + 1;
+            try tape.appendNTimes(0, n);
         }
+        // Return the value address
+        return &tape.items[addr.i];
     }
 
-    pub fn deinit(self: TuringTape) void {
-        for (self.all_blocks.items) |b| {
-            b.deinit(self.allocator);
-            self.allocator.destroy(b);
-        }
+    pub fn get(self: *TuringTape, idx: i128) !u8 {
+        const addrv = try self.getAddr(idx);
+        return addrv.*;
+    }
+
+    pub fn set(self: *TuringTape, idx: i128, v: u8) !void {
+        const addrv = try self.getAddr(idx);
+        addrv.* = v;
+    }
+
+    pub fn deinit(self: *TuringTape) void {
+        self.tape_rh.deinit();
+        self.tape_lh.deinit();
     }
 };
-
-test "TuringTapeBlock" {
-    const allocator = std.heap.page_allocator;
-    var ttb1 = TuringTapeBlock.init(0, 100);
-    try ttb1.build(allocator);
-    defer ttb1.deinit(allocator);
-    var ttb2 = TuringTapeBlock.init(100, 200);
-    try ttb2.build(allocator);
-    defer ttb2.deinit(allocator);
-
-    ttb1.next = &ttb2;
-
-    try testing.expect(ttb1.items.len == 100);
-    try testing.expect(ttb1.next.?.items.len == 100);
-}
 
 test "TuringTape" {
     const allocator = std.heap.page_allocator;
-    var tt = try TuringTape.init(allocator, 100);
+    var tt = TuringTape.init(allocator);
     defer tt.deinit();
 
-    try testing.expect(tt.idx == 0);
+    const a1 = TuringTape.tape_address(2);
+    const a2 = TuringTape.tape_address(-4);
 
-    try tt.appendBlock();
+    try testing.expect(a1.i == 2);
+    try testing.expect(!a1.is_left);
 
-    try testing.expect(tt.first_idx == 0);
-    try testing.expect(tt.last_idx == 100);
-    try testing.expect(tt.last_block.?.prev == null);
-    try testing.expect(tt.first_block.?.next == null);
+    try testing.expect(a2.i == 3);
+    try testing.expect(a2.is_left);
 
-    try tt.appendBlock();
+    const pv = try tt.getAddr(0);
 
-    try testing.expect(tt.first_idx == 0);
-    try testing.expect(tt.last_idx == 200);
-    try testing.expect(tt.last_block.?.prev == tt.first_block);
-    try testing.expect(tt.first_block.?.next == tt.last_block);
+    try testing.expect(pv.* == 0);
+    try testing.expect(tt.tape_rh.items.len == 1);
 
-    try tt.prependBlock();
+    const v = try tt.get(-3);
 
-    try testing.expect(tt.first_idx == -100);
-    try testing.expect(tt.last_idx == 200);
-    try testing.expect(tt.last_block.?.prev == tt.first_block.?.next);
+    try testing.expect(v == 0);
+    try testing.expect(tt.tape_lh.items.len == 3);
+
+    try tt.set(0, 1);
+
+    const v2 = try tt.get(0);
+
+    try testing.expect(v2 == 1);
 }
